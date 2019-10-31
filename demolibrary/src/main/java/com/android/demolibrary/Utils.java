d91caad1544e0286c540219864cd23d2c2972ca6
xupeng7835xp <1054440583@qq.com>
2019-10-31 10:46:19 +0800

    添加方法

--- demolibrary/src/main/java/com/android/demolibrary/Utils.java
+++ demolibrary/src/main/java/com/android/demolibrary/Utils.java
@@ -2,6 +2,7 @@ package com.android.demolibrary;
 
 /**
  * Created by 徐鹏 on 19/10/31.
+ * 工具类
  */
 
 public class Utils {
@@ -9,6 +10,12 @@ public class Utils {
     public static String DemoAlert() {
 
         return "我是一个帅到逆天的男人";
+
+    }
+
+    public static int DemoAdd(int i, int j) {
+
+        return i + j;
 
     }
 }
